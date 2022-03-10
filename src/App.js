cc0d6014309e90dc8763ca384b43520db727573f
Martyna <martynach@ymail.com>
2022-03-10 15:53:21 +0100

    zmiana kolorow glownych

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+<<<<<<< Updated upstream
 import logo from './logo.svg';
 import './App.css';
+=======
+import logo from "./logo.svg";
+import "./App.css";
+import ReactDom from "react-dom";
+>>>>>>> Stashed changes
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -14,12 +21,25 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+<<<<<<< Updated upstream
         >
         Martyna
+=======
+       >
+
+          Martyna
+
+>>>>>>> Stashed changes
         </a>
       </header>
+
+      <div className="App-header">
+        <h3>testujemy temacik</h3>
+        </div>
     </div>
+    
   );
+  
 }
 
 export default App;
