c4bafcb74b2c9aeabd7005bb660a6275cc2c8054
Adrien Cognac <adrian.kaniak@gmail.com>
2022-03-10 15:58:37 +0100

    ngfhg
    Co-Authored-By: MartynaC <martynach@ymail.com>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ADER
+          ADER <strong>strong ader </strong>
         </a>
       </header>
     </div>
